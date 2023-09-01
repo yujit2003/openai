09774cd31df23920ad90199a42193009481db0d9
MANASH <anandmanash321@gmail.com>
2023-09-01 19:16:33 +0530

    changed the form design

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,59 +1,59 @@
-import { useState } from 'react'
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
-import './utils/home.css'
-import './utils/input.js'
+import { useState } from "react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
 
+import { Link } from "react-router-dom";
 
 const API_KEY = "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";
-const systemMessage = { 
-  "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
-}
+const systemMessage = {
+  role: "system",
+  content:
+    "Explain things like you're talking to a software professional with 2 years of experience.",
+};
 
-const  Home = () => {
-  const [tcase, setTcase] = useState(0);
+const Home = () => {
+  const [testCase, setTestCase] = useState(0);
   const [title, setTitle] = useState("Title");
   const [steps, setSteps] = useState("");
   const [notes, setNotes] = useState("");
   const [result, setResult] = useState("");
-  const [allQuery,setAllQuery] = useState([]);
-
+  const [allQuery, setAllQuery] = useState([]);
 
   const handleForm = (e) => {
-    alert(tcase+" "+title+" "+steps+" "+notes+" "+result)
-
-    setTcase(1+tcase);
-    setTitle("");
-    setSteps("");
-    setNotes("");
-    setResult("");
-  }
+  
+  };
   const [messages, setMessages] = useState([
     {
       message: "Hello, I'm Prompt GPT! Ask me anything!",
       sentTime: "just now",
-      sender: "ChatGPT"
-    }
+      sender: "ChatGPT",
+    },
   ]);
   const [isTyping, setIsTyping] = useState(false);
 
   const handleSend = async (message) => {
     const newMessage = {
       message,
-      direction: 'outgoing',
-      sender: "user"
+      direction: "outgoing",
+      sender: "user",
     };
 
     const newMessages = [...messages, newMessage];
-    
+
     setMessages(newMessages);
 
     setIsTyping(true);
     await processMessageToChatGPT(newMessages);
   };
 
-  async function processMessageToChatGPT(chatMessages) { 
-
+  async function processMessageToChatGPT(chatMessages) {
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
       if (messageObject.sender === "ChatGPT") {
@@ -61,79 +61,182 @@ const  Home = () => {
       } else {
         role = "user";
       }
-      return { role: role, content: messageObject.message}
+      return { role: role, content: messageObject.message };
     });
 
     const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        systemMessage,  
-        ...apiMessages 
-      ]
-    }
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
 
-    await fetch("https://api.openai.com/v1/chat/completions", 
-    {
+    await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
-        "Authorization": "Bearer " + API_KEY,
-        "Content-Type": "application/json"
+        Authorization: "Bearer " + API_KEY,
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(apiRequestBody)
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
-      setMessages([...chatMessages, {
-        message: data.choices[0].message.content,
-        sender: "ChatGPT"
-      }]);
-      setIsTyping(false);
-    });
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        setMessages([
+          ...chatMessages,
+          {
+            message: data.choices[0].message.content,
+            sender: "ChatGPT",
+          },
+        ]);
+        setIsTyping(false);
+      });
   }
 
   return (
     <>
-      <div class="container">
-        <div class="left-panel">
-            <div class="top-bar">
-                <div class="circle"></div>
-                <div class="circle"></div>
-                <div class="circle"></div>
+      <section className="bg-gray-50 dark:bg-gray-900 min-h-screen">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
+          <div className="flex flex-col justify-center">
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+              We invest in the world's potential
+            </h1>
+            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+              Here at Our application name we focus on markets where technology, innovation,
+              and capital can unlock long-term value and drive economic growth.
+            </p>
+            <Link
+              to="/"
+              className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center"
+            >
+              Read more about our app
+              <svg
+                className="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="/"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </Link>
+          </div>
+          <div>
+            <div className="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
+              <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+                Try our webapp
+              </h2>
+              <div className="mt-8 space-y-6">
+                <div>
+                  <label
+                    htmlFor="testCase"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Test Case Id
+                  </label>
+                  <input
+                    type="testCase"
+                    name="testCase"
+                    id="testCase"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   
+                    required
+                    value={testCase}
+                    onChange={(e) => setTestCase(e.target.value)}
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="title"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Title
+                  </label>
+                  <input
+                    type="title"
+                    name="title"
+                    id="title"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="steps"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Steps
+                  </label>
+                  <textarea
+                    type="steps"
+                    name="steps"
+                    id="steps"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                    value={steps}
+                    onChange={(e) => setSteps(e.target.value)}
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="steps"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Notes
+                  </label>
+                  <textarea
+                    type="notes"
+                    name="notes"
+                    id="notes"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                    value={notes}
+                    onChange={(e) => setNotes(e.target.value)}
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="steps"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Expected Result
+                  </label>
+                  <textarea
+                    type="result"
+                    name="result"
+                    id="result"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                    value={result}
+                    onChange={(e) => setResult(e.target.value)}
+                  />
+                </div>
+
+                <div className="flex items-start"></div>
+                <button
+                  type="submit"
+                  className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Submit
+                </button>
+               
+              </div>
             </div>
-            <div class="form">
-                 <div class="form__group field">
-                  <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
-                  <label for="name" class="form__label">Test ID CASE</label>
-                </div>
-                 <div class="form__group field">
-                  <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
-                  <label for="name" class="form__label">TITLE</label>
-                </div>
-                <div class="form__group field">
-                  <textarea type="input" class="form__field" placeholder="Name" name="name" id='name' required />
-                  <label for="name" class="form__label">STEPS</label>
-                </div>
-                <div class="form__group field">
-                  <textarea type="input" class="form__field" placeholder="Name" name="name" id='name' required />
-                  <label for="name" class="form__label">NOTES</label>
-                </div>
-            </div>
-            <div class="submit-button">
-                <button type="button" class="btn">Submit</button>
-            </div>
+          </div>
         </div>
-        <div class="right-panel">
-            <div class="expected-result-header">
-                Expected Result
-            </div>
-            <div class="form__group field">
-                  <textarea type="input" class="form__field" placeholder="Name" name="name" id='name' required />
-                  <label for="name" class="form__label">Expected Result...</label>
-                </div>
-        </div>
-    </div>
-    </>   
-            
-  )
-}
-export default Home
+      </section>
+    </>
+  );
+};
+export default Home;
